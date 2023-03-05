5a384f8fca5d0a3bcf71c6dea53e027c98ab8ce0
VladHt <vl.vlad@mail.ru>
2023-03-05 19:13:30 +0300

    add files

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import './styles/main.scss'
 
 document.getElementById('btn-toggle').onclick = function() {
     document.getElementById('body').classList.toggle('dark');
+    document.getElementById('html').classList.toggle('dark');
   }
