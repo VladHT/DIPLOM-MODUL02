3afd9692dd91fa26243671d13a7b382247b32932
VladHt <vl.vlad@mail.ru>
2023-03-17 22:44:58 +0300

    add files

--- src/index.js
+++ src/index.js
@@ -5,3 +5,22 @@ document.getElementById('btn-toggle').onclick = function() {
     document.getElementById('body').classList.toggle('dark');
     document.getElementById('html').classList.toggle('dark');
   }
+
+  document.getElementById('menu_show').onclick = function() {
+  document.getElementById("menu").classList.toggle("show");
+}
+
+const links = document.querySelectorAll(".smooz");
+
+       for (const link of links) {
+       link.addEventListener("click", clickHandler);
+       }
+
+       function clickHandler(e) {
+       e.preventDefault();
+       const href = this.getAttribute("href");
+
+       document.querySelector(href).scrollIntoView({
+           behavior: "smooth"
+       });
+       }
